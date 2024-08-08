60eeba0ad82aec7a0a60bf03f5362a93434b5ec0
benkoppe <bentastic1@gmail.com>
2024-08-08 09:55:07 -0700

    update to latest 8-8-2024

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -253,6 +253,74 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+    },
+  },
+
+  -- Code Runner plugin
+  {
+    'CRAG666/code_runner.nvim',
+    opts = {
+      filetype = {
+        java = {
+          'cd $dir &&',
+          'javac $fileName &&',
+          'java $fileNameWithoutExt',
+        },
+        python = 'python3 -u',
+        typescript = 'deno run',
+        rust = {
+          'cd $dir &&',
+          'rustc $fileName &&',
+          '$dir/$fileNameWithoutExt',
+        },
+      },
+    },
+    config = function()
+      require('code_runner').setup()
+
+      vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+      vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+      vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+      vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+      vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+      vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+      vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+    end,
+  },
+
+  -- ts-error-translator plugin
+  { 'dmmulroy/ts-error-translator.nvim' },
+
+  -- Comment.nvim
+  { 'numToStr/Comment.nvim' },
+
+  -- Oil.nvim
+  { 'stevearc/oil.nvim', dependencies = { { 'echasnovski/mini.icons', opts = {} } } },
+
+  -- toggleterm.nvim
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
+
+  { 'github/copilot.vim' },
+
+  -- flash.nvim
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        search = {
+          enabled = true,
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" }
     },
   },
 
@@ -880,12 +948,13 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.neoscroll', -- neoscroll support
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
